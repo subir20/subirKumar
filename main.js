bcebb0eabac8db1b412dcacf5991269d023bb804
Subir Kumar <subirpratihar2000@gmail.com>
2023-04-03 12:19:10 +0530

    dark mode done

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log('Hello World!');
 
 let inputBox = document.querySelectorAll(".input-box");
+let mode = document.querySelectorAll(".mode")[0];
 
 for (let i = 0; i < 4; i++) {
     inputBox[i].addEventListener("focusin",function(){
@@ -16,6 +17,23 @@ for (let i = 0; i < 4; i++) {
         inputBox[i].children[0].style.color=""
         inputBox[0].children[0].style.backgroundColor="none"
     })
-
-    
 }
+
+mode.onclick = function(){
+    let light = mode.children[0].classList;
+    light.toggle("fa-moon");
+
+    document.body.classList.toggle("dark-mode");
+     
+    if (light[2] == "fa-moon") {
+        mode.style.justifyContent="flex-end";
+        mode.children[0].style.color="white";
+        
+    } else {
+        // console.log("thsn")
+        mode.children[0].style.color=""
+        mode.style.justifyContent="flex-start";
+        
+    }
+}
+
